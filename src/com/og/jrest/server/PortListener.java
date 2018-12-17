6695533d1a757066bda82e5f303f4147cd211998
Matthew Shoemaker <shoemaker.277@osu.edu>
2018-12-17 15:24:36 -0500

    Created a tentative file structure for some components

---
+++ src/com/og/jrest/server/PortListener.java
@@ -0,0 +1,9 @@
+package com.og.jrest.server;
+
+public class PortListener {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/com/og/jrest/server/RequestHandler.java
@@ -0,0 +1,11 @@
+package com.og.jrest.server;
+
+public class RequestHandler implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
