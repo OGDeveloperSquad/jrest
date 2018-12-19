f8d2e0c5b05209fd7deb98497d440f1135b20afe
Hesham <Hussain.119@osu.edu>
2018-12-19 12:48:51 -0500

    Request and tests for it

--- src/com/og/jrest/http/Request.java
+++ src/com/og/jrest/http/Request.java
@@ -2,22 +2,92 @@ package com.og.jrest.http;
 
 import com.og.jrest.logging.Log;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * This class is a model of an HTTP message. Contains things like Headers,
  * method (i.e. GET, POST, PUT, or DELETE), and request body. Pretty
  * straightforward
  * 
  * @author matthew.shoemaker
+ * @author Hussain
  *
  */
 public class Request {
 
-	public Request() {
-		// Just for fun
+	
+	public String verb;
+	public String uri;
+	public String httpVersion;
+	public Map<String, String[]> headers;
+	public String body;
+	
+	
+	
+	public Request(String httpRequest ) {
+		
+		this.headers = new HashMap<String, String[]>();
+		this.parseRequest(httpRequest);
 		Log.debug("New request class instantiated!");
+		
+	}
+	
+	
+	/*
+	 * 
+	 * Parses a given string http request
+	 * 
+	 * @param httpRequest
+	 * 
+	 */
+	private void  parseRequest(String httpRequest) {
+		Scanner req  = new Scanner(httpRequest);
+
+		//parse request line assuming format request-method-name request-URI HTTP-version
+		this.verb = req.next();
+		this.uri = req.next();
+		this.httpVersion = req.next();
+
+		req.nextLine();
+
+		//parse request headers into map
+		String headerLine = req.nextLine();
+		while (!headerLine.equals("")){
+			
+			int colonI = headerLine.indexOf(":");
+			String name = headerLine.substring(0, colonI);
+			String vals = headerLine.substring(colonI + 1);
+
+			String[] values = vals.split(",");
+			
+			//trim values of whitespaces
+			for(int i = 0; i < values.length; i++) {
+				values[i] = values[i].trim();
+			}
+
+			this.headers.put(name, values);
+
+			headerLine = req.nextLine();
+			
+		}
+
+		String body = "";
+		while(req.hasNext()){
+			body = body + req.nextLine();
+		}
+		
+		this.body = body;
+
+		
+	
+
+
+		req.close();
 	}
 
-	public String host;
-	public String verb = "GET";
+
+	
 
 }
---
+++ test/com/og/jrest/test/http/RequestTest.java
@@ -0,0 +1,65 @@
+package com.og.jrest.test.http;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.og.jrest.http.Request;
+
+class RequestTest {
+
+	@Test
+	void postHTMLBody() {
+		String testRequest = "POST /cgi-bin/process.cgi HTTP/1.1\n";
+        testRequest = testRequest + "User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)\n";
+        testRequest = testRequest + "Accept-Encoding: gzip, deflate\n";
+        testRequest = testRequest + "Connection: Keep-Alive\n\n";
+        testRequest = testRequest + "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" + 
+        		"<string xmlns=\"http://clearforest.com/\">string</string>";
+
+        Request test = new Request(testRequest);
+        
+        //test request line
+        assertEquals( "POST", test.verb);
+        assertEquals("/cgi-bin/process.cgi", test.uri );
+        assertEquals( "HTTP/1.1", test.httpVersion);
+        //test headers
+        assertEquals( true, test.headers.containsKey("User-Agent"));
+        assertEquals( true, test.headers.containsKey("Accept-Encoding"));
+        assertEquals( true, test.headers.containsKey("Connection"));
+        assertEquals("Mozilla/4.0 (compatible; MSIE5.01; Windows NT)", test.headers.get("User-Agent")[0]);
+        assertEquals("gzip", test.headers.get("Accept-Encoding")[0]);
+        assertEquals("deflate", test.headers.get("Accept-Encoding")[1]);
+        assertEquals("Keep-Alive", test.headers.get("Connection")[0]);
+        //test body
+        assertEquals("<?xml version=\"1.0\" encoding=\"utf-8\"?>" + 
+        		"<string xmlns=\"http://clearforest.com/\">string</string>", test.body );
+	}
+	
+	@Test
+	void postEmptyBody() {
+		String testRequest = "POST /cgi-bin/process.cgi HTTP/1.1\n";
+        testRequest = testRequest + "User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)\n";
+        testRequest = testRequest + "Accept-Encoding: gzip, deflate\n";
+        testRequest = testRequest + "Connection: Keep-Alive\n\n";
+        
+
+        Request test = new Request(testRequest);
+        
+        //test request line
+        assertEquals( "POST", test.verb);
+        assertEquals("/cgi-bin/process.cgi", test.uri );
+        assertEquals( "HTTP/1.1", test.httpVersion);
+        //test headers
+        assertEquals( true, test.headers.containsKey("User-Agent"));
+        assertEquals( true, test.headers.containsKey("Accept-Encoding"));
+        assertEquals( true, test.headers.containsKey("Connection"));
+        assertEquals("Mozilla/4.0 (compatible; MSIE5.01; Windows NT)", test.headers.get("User-Agent")[0]);
+        assertEquals("gzip", test.headers.get("Accept-Encoding")[0]);
+        assertEquals("deflate", test.headers.get("Accept-Encoding")[1]);
+        assertEquals("Keep-Alive", test.headers.get("Connection")[0]);
+        //test body
+        assertEquals("", test.body );
+	}
+
+}
