4ef4f672186087e2f277fe6b07a8ee0602e1411b
Matthew Shoemaker <shoemaker.277@osu.edu>
2018-12-20 09:10:31 -0500

    Set up a working listener and handler

--- src/com/og/jrest/server/RequestHandler.java
+++ src/com/og/jrest/server/RequestHandler.java
@@ -2,11 +2,11 @@ package com.og.jrest.server;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+import com.og.jrest.http.Request;
 import com.og.jrest.logging.Log;
 
 /**
@@ -22,46 +22,89 @@ public class RequestHandler implements Runnable {
 	private Socket socket;
 	private int clientNumber;
 
-   
 	public RequestHandler(Socket socket, int clientNumber) {
 		this.socket = socket;
-        this.clientNumber = clientNumber;
+		this.clientNumber = clientNumber;
 	}
 
-
 	/**
-     * Services this thread's client by first sending the
-     * client a welcome message then repeatedly reading strings
-     * and sending back the capitalized version of the string.
-     */
-    public void run() {
-        try {
-        	
-            // Decorate the streams so we can send characters
-            // and not just bytes.  Ensure output is flushed
-            // after every newline.
-          //  BufferedReader in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-            PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
-            
-            InputStream readRequest = socket.getInputStream();
-            
-           
-            /*this will need manual input or a file to get things. How to test a file??*/
-            byte[] buf = new byte[4096];
-            readRequest.read(buf);
-        
-            String httpPayload = new String(buf, "UTF-8");
+	 * Services this thread's client by first sending the client a welcome message
+	 * then repeatedly reading strings and sending back the capitalized version of
+	 * the string.
+	 */
+	public void run() {
+		try {
 
-            Log.debug("....handling client");
-            /*
-             * DO something with HTTP request. 
-             * 
-             * 
-             */
-            
-        } catch (IOException e) {
-            Log.debug("Error handling client");
-        }
-    }
-    
+			// Decorate the streams so we can send characters
+			// and not just bytes. Ensure output is flushed
+			// after every newline.
+			// BufferedReader in = new BufferedReader(new
+			// InputStreamReader(this.socket.getInputStream()));
+			// InputStream readRequest = socket.getInputStream();
+			// /*this will need manual input or a file to get things. How to test a file??*/
+			// byte[] buf = new byte[4096];
+			// readRequest.read(buf);
+			//
+			// String httpPayload = new String(buf, "UTF-8");
+
+			/*****************************************************************************************
+			 * How about this instead?
+			 *****************************************************************************************/
+			BufferedReader in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+			PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
+
+			/*
+			 * The way input stream works is kinda dumb, becuase we can only use readLine()
+			 * until we get an empty line. This causes problems because every http request
+			 * has a blank line between the reqeust body and the headers. So we have to read
+			 * the request headers first, looking for Content-Length header which tells you
+			 * the exact length of the request body. Then, if the length of the request body
+			 * is greater than 0, we have to parse the request body using a character array
+			 * and then converting that to a string. This took a lot of googling to figure
+			 * out lol.
+			 */
+			int bodyLength = 0;
+
+			// Read the header lines
+			String line = in.readLine();
+			String httpRaw = line;
+			while (line != null && line.length() > 0) {
+				httpRaw += line + "\n";
+				line = in.readLine();
+				/*
+				 * Check to see if this line is the Content-Length header, and if so, get the
+				 * content length and save to bodyLength
+				 */
+				if (line.indexOf("Content-Length:") > -1) {
+					bodyLength = new Integer(line.substring(line.indexOf("Content-Length:") + 16, line.length()))
+							.intValue();
+				}
+			}
+
+			/*
+			 * If the request has a body, we have to read it into a character array then
+			 * into a string.
+			 */
+			if (bodyLength > 0) {
+				char[] charArray = new char[bodyLength];
+				in.read(charArray, 0, bodyLength);
+				httpRaw = "\n" + new String(charArray);
+			}
+
+			// Making the request object
+			Request request = new Request(httpRaw);
+
+			// Just echoing back the reqeust for now. This is just for fun until we actually
+			// make it do something lol
+			String responseHeader = "HTTP/1.1 200 OK \n\n";
+			out.write(responseHeader + httpRaw);
+
+			out.flush();
+			out.close();
+			this.socket.close();
+		} catch (IOException e) {
+			Log.exception(e);
+		}
+	}
+
 }
--- src/com/og/jrest/server/RequestListener.java
+++ src/com/og/jrest/server/RequestListener.java
@@ -2,9 +2,9 @@ package com.og.jrest.server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 import com.og.jrest.logging.Log;
-
 
 /**
  * This is the entry point for the server. This will continuously listen on a
@@ -17,35 +17,39 @@ public class RequestListener {
 
 	public static void main(String[] args) throws IOException {
 		int clientNumber = 0;
-//		Log.info("");
-//		Log.setAllOutput(System.out);
-//		Log.error("");
-//		Log.exception(new Exception());
-//		Log.debug(""); 
+		// Log.info("");
+		// Log.setAllOutput(System.out);
+		// Log.error("");
+		// Log.exception(new Exception());
+		// Log.debug("");
 
-	
-		Log.info("Oh hey the program has begun (btw this is a local log)");
+		Log.info("Oh hey the program has begun ");
 
 		// Just listen forever!
 		while (true) {
-			 clientNumber++;
+			clientNumber++;
 			// TODO -- Use ServerSocket to listen on any port you like and wait for a
 			// request to come in, then do something cool with it like hand off the request
 			// to some kind of request handler class
-			  ServerSocket listener = new ServerSocket(9090);
-			 try {
-		            Log.info(" Getting new thread for client #." + clientNumber);	
-		            
-		             new Thread(new RequestHandler(listener.accept(),clientNumber)).start();
-		             Log.info(".... Finished thread");
-		             
-		            
-		        } finally {
-		            listener.close();
-		            Log.info("Connection with client #" + clientNumber + " closed");
-		        }
+			ServerSocket listener = new ServerSocket(9091);
+			try {
+				Log.info(" Getting new thread for client #." + clientNumber);
+
+				// Don't daisy-chain things together like this. Makes debugging difficult and
+				// its really hard to read. Generally frowned upon
+				// new Thread(new RequestHandler(listener.accept(), clientNumber)).start();
+
+				Socket socket = listener.accept();
+				listener.close();
+				RequestHandler handler = new RequestHandler(socket, clientNumber);
+				Thread thread = new Thread(handler);
+				thread.start();
+
+			} finally {
+				listener.close();
+				Log.info("Connection with client #" + clientNumber + " closed");
 			}
 		}
-
 	}
 
+}
